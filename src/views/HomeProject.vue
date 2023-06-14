fc56f8657fe40830b8090a34b7bae3cdba477b28
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-06-14 21:10:51 +0200

    Added social links

--- src/views/HomeProject.vue
+++ src/views/HomeProject.vue
@@ -8,7 +8,25 @@
                 <h2 class="display-5 fw-bold title-color">
                     Welcome to my Portfolio
                 </h2>
+                <ul class="link-ul d-flex align-content-end justify-content-end ">
+                    <li>
+                        <a href="https://www.instagram.com/alessandro.ap__">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </li>
 
+                    <li class="mx-3">
+                        <a href="https://www.linkedin.com/in/alessandro-pagliuca-933a801b3">
+                            <i class="fa-brands fa-linkedin"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="https://github.com/AlessandroPagliuca">
+                            <i class="fa-brands fa-square-github"></i>
+                        </a>
+                    </li>
+                </ul>
             </section>
 
         </div>
